eeeee7769aa1b1b7311d9423a17151c527d8ed1b
Cullen Rombach <cullenrombach@gmail.com>
2021-12-10 14:28:54 -0500

    Add gaming layer

--- keyboards/aleblazer/zodiark/keymaps/crombach/keymap.c
+++ keyboards/aleblazer/zodiark/keymaps/crombach/keymap.c
@@ -16,7 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 enum sofle_layers {
     /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
     _QWERTY,
-    _COLEMAK,
+    _GAMING,
     _LOWER,
     _RAISE,
     _ADJUST,
@@ -24,7 +24,7 @@ enum sofle_layers {
 
 enum custom_keycodes {
     KC_QWERTY = SAFE_RANGE,
-    KC_COLEMAK,
+    KC_GAMING,
     KC_LOWER,
     KC_RAISE,
     KC_ADJUST,
@@ -38,11 +38,19 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_QWERTY] = LAYOUT(
-      GRAVE_ESC, KC_1,    KC_2,    KC_3,    KC_4,     KC_5,                                       KC_6, KC_7,     KC_8,      KC_9,    KC_0,    KC_BSLS,
-      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,     KC_T, KC_LBRC,                     KC_RBRC, KC_Y, KC_U,     KC_I,      KC_O,    KC_P,    KC_BSPC,
-      KC_CAPS,   KC_A,    KC_S,    KC_D,    KC_F,     KC_G, KC_MINS,                     KC_EQL,  KC_H, KC_J,     KC_K,      KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,     KC_B, KC_GRV, KC_MUTE,    RGB_TOG, KC_DEL,  KC_N, KC_M,     KC_COMM,   KC_DOT,  KC_SLSH, KC_RSFT,
-      KC_LCTL,   KC_LGUI, KC_LALT, _______, KC_LOWER, /**/  KC_SPC, KC_ENT,     KC_ENT,  KC_SPC,  /**/  KC_RAISE, KC_ADJUST, KC_RALT, KC_RGUI, KC_RCTL
+      GRAVE_ESC, KC_1,    KC_2,    KC_3,      KC_4,     KC_5,                                       KC_6, KC_7,     KC_8,      KC_9,    KC_0,    KC_BSLS,
+      KC_TAB,    KC_Q,    KC_W,    KC_E,      KC_R,     KC_T, KC_LBRC,                     KC_RBRC, KC_Y, KC_U,     KC_I,      KC_O,    KC_P,    KC_BSPC,
+      KC_CAPS,   KC_A,    KC_S,    KC_D,      KC_F,     KC_G, KC_MINS,                     KC_EQL,  KC_H, KC_J,     KC_K,      KC_L,    KC_SCLN, KC_QUOT,
+      KC_LSFT,   KC_Z,    KC_X,    KC_C,      KC_V,     KC_B, KC_GRV, KC_MUTE,    RGB_TOG, KC_DEL,  KC_N, KC_M,     KC_COMM,   KC_DOT,  KC_SLSH, KC_RSFT,
+      KC_LCTL,   KC_LGUI, KC_LALT, KC_ADJUST, KC_LOWER, /**/  KC_SPC, KC_ENT,     KC_ENT,  KC_SPC,  /**/  KC_RAISE, KC_ADJUST, KC_RALT, KC_RGUI, KC_RCTL
+    ),
+
+    [_GAMING] = LAYOUT(
+      GRAVE_ESC, KC_1,    KC_2,    KC_3,      KC_4,     KC_5,                                       KC_6, KC_7,     KC_8,      KC_9,    KC_0,    KC_BSLS,
+      KC_TAB,    KC_Q,    KC_W,    KC_E,      KC_R,     KC_T, KC_LBRC,                     KC_RBRC, KC_Y, KC_U,     KC_I,      KC_O,    KC_P,    KC_BSPC,
+      KC_CAPS,   KC_A,    KC_S,    KC_D,      KC_F,     KC_G, KC_MINS,                     KC_EQL,  KC_H, KC_J,     KC_K,      KC_L,    KC_SCLN, KC_QUOT,
+      KC_LSFT,   KC_Z,    KC_X,    KC_C,      KC_V,     KC_B, KC_GRV, KC_MUTE,    RGB_TOG, KC_DEL,  KC_N, KC_M,     KC_COMM,   KC_DOT,  KC_UP,   KC_RSFT,
+      KC_LCTL,   KC_LGUI, KC_LALT, KC_ADJUST, KC_LOWER, /**/  KC_SPC, KC_ENT,     KC_ENT,  KC_SPC,  /**/  KC_RAISE, KC_ADJUST, KC_LEFT, KC_DOWN, KC_RIGHT
     ),
 
 	[_LOWER] = LAYOUT(
@@ -62,11 +70,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       ),
 
 	[_ADJUST] = LAYOUT(
-      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                          KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-      _______, _______, _______, _______, _______, _______, _______,                      _______, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, KC_F12,
-      _______, _______, _______, _______, _______, _______, _______,                      _______, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______, _______,
-      CG_TOGG, _______, _______, _______, _______, /*   */  _______, _______,    _______, _______, /*   */  _______, _______, _______, _______, _______
+      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                            KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+      _______, _______, _______, _______, _______, _______, KC_QWERTY,                      _______, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, KC_F12,
+      _______, _______, _______, _______, _______, _______, KC_GAMING,                      _______, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______,   _______,    _______, _______, _______, _______, _______, _______, _______, _______,
+      CG_TOGG, _______, _______, _______, _______, /*   */  _______,   _______,    _______, _______, /*   */  _______, _______, _______, _______, CG_TOGG
       )
 
 };
@@ -103,13 +111,13 @@ static void print_status_narrow(void) {
     oled_set_cursor(0, 5);
     switch (get_highest_layer(default_layer_state)) {
         case _QWERTY:
-            oled_write_P(PSTR("Qwerty "), false);
+            oled_write_P(PSTR("Qwerty"), false);
             break;
-        case _COLEMAK:
-            oled_write_P(PSTR("Colemak"), false);
+        case _GAMING:
+            oled_write_P(PSTR("Gaming"), false);
             break;
         default:
-            oled_write_P(PSTR("Undef  "), false);
+            oled_write_P(PSTR("Undef "), false);
     }
 
     /* Print current layer */
@@ -117,7 +125,6 @@ static void print_status_narrow(void) {
     oled_write("LAYER", false);
     oled_set_cursor(0, 9);
     switch (get_highest_layer(layer_state)) {
-        case _COLEMAK:
         case _QWERTY:
             oled_write_P(PSTR("Base "), false);
             break;
@@ -168,9 +175,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 set_single_persistent_default_layer(_QWERTY);
             }
             return false;
-        case KC_COLEMAK:
+        case KC_GAMING:
             if (record->event.pressed) {
-                set_single_persistent_default_layer(_COLEMAK);
+                set_single_persistent_default_layer(_GAMING);
             }
             return false;
         case KC_LOWER:
