a54edb2c50eef4e211091ed4146dd7994cd815dd
Cullen Rombach <cullenrombach@gmail.com>
2023-02-04 11:07:50 -0500

    Use new grave esc keycode in Zodiark keymap

--- keyboards/aleblazer/zodiark/keymaps/crombach/keymap.c
+++ keyboards/aleblazer/zodiark/keymaps/crombach/keymap.c
@@ -38,19 +38,19 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_QWERTY] = LAYOUT(
-      GRAVE_ESC, KC_1,    KC_2,    KC_3,      KC_4,     KC_5,                                       KC_6, KC_7,     KC_8,      KC_9,    KC_0,    KC_BSLS,
-      KC_TAB,    KC_Q,    KC_W,    KC_E,      KC_R,     KC_T, KC_LBRC,                     KC_RBRC, KC_Y, KC_U,     KC_I,      KC_O,    KC_P,    KC_BSPC,
-      KC_CAPS,   KC_A,    KC_S,    KC_D,      KC_F,     KC_G, KC_MINS,                     KC_EQL,  KC_H, KC_J,     KC_K,      KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSFT,   KC_Z,    KC_X,    KC_C,      KC_V,     KC_B, KC_GRV, KC_MUTE,    RGB_TOG, KC_DEL,  KC_N, KC_M,     KC_COMM,   KC_DOT,  KC_SLSH, KC_RSFT,
-      KC_LCTL,   KC_LGUI, KC_LALT, KC_ADJUST, KC_LOWER, /**/  KC_SPC, KC_ENT,     KC_ENT,  KC_SPC,  /**/  KC_RAISE, KC_ADJUST, KC_RALT, KC_RGUI, KC_RCTL
+      QK_GESC, KC_1,    KC_2,    KC_3,      KC_4,     KC_5,                                       KC_6, KC_7,     KC_8,      KC_9,    KC_0,    KC_BSLS,
+      KC_TAB,  KC_Q,    KC_W,    KC_E,      KC_R,     KC_T, KC_LBRC,                     KC_RBRC, KC_Y, KC_U,     KC_I,      KC_O,    KC_P,    KC_BSPC,
+      KC_CAPS, KC_A,    KC_S,    KC_D,      KC_F,     KC_G, KC_MINS,                     KC_EQL,  KC_H, KC_J,     KC_K,      KC_L,    KC_SCLN, KC_QUOT,
+      KC_LSFT, KC_Z,    KC_X,    KC_C,      KC_V,     KC_B, KC_GRV, KC_MUTE,    RGB_TOG, KC_DEL,  KC_N, KC_M,     KC_COMM,   KC_DOT,  KC_SLSH, KC_RSFT,
+      KC_LCTL, KC_LGUI, KC_LALT, KC_ADJUST, KC_LOWER, /**/  KC_SPC, KC_ENT,     KC_ENT,  KC_SPC,  /**/  KC_RAISE, KC_ADJUST, KC_RALT, KC_RGUI, KC_RCTL
     ),
 
     [_GAMING] = LAYOUT(
-      GRAVE_ESC, KC_1,    KC_2,    KC_3,      KC_4,     KC_5,                                       KC_6, KC_7,     KC_8,      KC_9,    KC_0,    KC_BSLS,
-      KC_TAB,    KC_Q,    KC_W,    KC_E,      KC_R,     KC_T, KC_LBRC,                     KC_RBRC, KC_Y, KC_U,     KC_I,      KC_O,    KC_P,    KC_BSPC,
-      KC_CAPS,   KC_A,    KC_S,    KC_D,      KC_F,     KC_G, KC_MINS,                     KC_EQL,  KC_H, KC_J,     KC_K,      KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSFT,   KC_Z,    KC_X,    KC_C,      KC_V,     KC_B, KC_GRV, KC_MUTE,    RGB_TOG, KC_DEL,  KC_N, KC_M,     KC_COMM,   KC_DOT,  KC_UP,   KC_RSFT,
-      KC_LCTL,   KC_LGUI, KC_LALT, KC_ADJUST, KC_LOWER, /**/  KC_SPC, KC_ENT,     KC_ENT,  KC_SPC,  /**/  KC_RAISE, KC_ADJUST, KC_LEFT, KC_DOWN, KC_RIGHT
+      QK_GESC, KC_1,    KC_2,    KC_3,      KC_4,     KC_5,                                       KC_6, KC_7,     KC_8,      KC_9,    KC_0,    KC_BSLS,
+      KC_TAB,  KC_Q,    KC_W,    KC_E,      KC_R,     KC_T, KC_LBRC,                     KC_RBRC, KC_Y, KC_U,     KC_I,      KC_O,    KC_P,    KC_BSPC,
+      KC_CAPS, KC_A,    KC_S,    KC_D,      KC_F,     KC_G, KC_MINS,                     KC_EQL,  KC_H, KC_J,     KC_K,      KC_L,    KC_SCLN, KC_QUOT,
+      KC_LSFT, KC_Z,    KC_X,    KC_C,      KC_V,     KC_B, KC_GRV, KC_MUTE,    RGB_TOG, KC_DEL,  KC_N, KC_M,     KC_COMM,   KC_DOT,  KC_UP,   KC_RSFT,
+      KC_LCTL, KC_LGUI, KC_LALT, KC_ADJUST, KC_LOWER, /**/  KC_SPC, KC_ENT,     KC_ENT,  KC_SPC,  /**/  KC_RAISE, KC_ADJUST, KC_LEFT, KC_DOWN, KC_RIGHT
     ),
 
 	[_LOWER] = LAYOUT(
